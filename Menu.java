d60c5eedac42a3adfaa8acad200e41cce3589ee8
Gideon Goderski <g.goderski@workingspirit.nl>
2023-01-31 18:35:02 +0100

    derde menu

--- Menu.java
+++ Menu.java
@@ -1,34 +1,41 @@
 import java.util.Scanner;
 
 public class Menu {
-  
+  public static void displayMenu() {
+    System.out.println("Press 1 to input a new book");
+    System.out.println("Press 2 to view all books in the library");
+    System.out.println("Press 3 to delete a book from the library");
+    System.out.println("Press 0 to exit");
+  }
+
+  public static int getChoice() {
     Scanner sc = new Scanner(System.in);
-    int keuze;
+    System.out.print("Enter your choice: ");
+    return sc.nextInt();
+  }
 
+  public static void main(String[] args) {
+    int choice;
     do {
-      System.out.println("Press 1 to input a new book");
-      System.out.println("Press 2 to view all books in the library");
-      System.out.println("Press 3 to delete a book from the library");
-      System.out.println("Press 0 to exit");
-      System.out.print("Enter your choice: ");
-      choice = sc.nextInt();
+      displayMenu();
+      choice = getChoice();
 
-      switch (keuze) {
+      switch (choice) {
         case 1:
-          System.out.println("You selected option 1: Input a new book");
+          System.out.println("You can input a new book");
           break;
         case 2:
-          System.out.println("You selected option 2: View all books in the library");
+          System.out.println("You can view here all the books in the library");
           break;
         case 3:
-          System.out.println("You selected option 3: Delete a book from the library");
+          System.out.println("You can delete a book from the library");
           break;
         case 0:
           System.out.println("Exiting");
           break;
         default:
-          System.out.println("Invalid choice. Please try again.");
+          System.out.println("Please enter a correct number!");
       }
-    } while (keuze != 0);
-  
+    } while (choice != 0);
+  }
 }
