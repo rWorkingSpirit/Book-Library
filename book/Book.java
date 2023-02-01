114a33148d9e3f275eb7bcc71ac355a094053973
Gideon Goderski <g.goderski@workingspirit.nl>
2023-02-01 12:54:54 +0100

    first book

---
+++ book/Book.java
@@ -0,0 +1,12 @@
+package book;
+
+
+public class Book {
+
+	private String title;
+	private String author;
+	private String isbn;
+	private String published;
+	private String cover;
+	private String category;
+}
